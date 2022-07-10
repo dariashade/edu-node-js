d3e40e47349779c3a84c77d81d8916831d02a67c
dariashade <daria.lipinskaya@gmail.com>
2022-07-10 16:47:51 +0300

    Переменные окружения

---
+++ services/api.service.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+
+const getWeather = async (city) => {
+
+	const token = await getKeyValue(TOKEN_DICTIONARY.token);
+	if (!token) {
+		throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
+	}
+	
+	const { data } = await axios.get('http://api.openweathermap.org/geo/1.0/direct', {
+		params: {
+			q: city,
+			appid: token,
+			limit: 1
+		}
+	});
+
+	return data;
+
+};
+
+export { getWeather };
--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+	if(!token.length) {
+		printError('Не передан токен');
+		return;
+	} 
 	try {
-		await saveKeyValue('token', token);
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess('Токен сохранен');
 	} catch (err) {
 		printError(err.message);
@@ -14,7 +19,7 @@ const saveToken = async (token) => {
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
-	
+	console.log(process.env);
 	if (args.h) {
 		printHelp();
 	}
@@ -28,7 +33,7 @@ const initCLI = () => {
 	}
 
 	// Вывести погоду
-
+	getWeather('Moscow');
 };
 
 initCLI();
