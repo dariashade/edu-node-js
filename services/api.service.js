cc0d6bd2bea731147490867088590383cff3c6cb
dariashade <daria.lipinskaya@gmail.com>
2022-07-10 17:53:01 +0300

    Обработка ошибок

--- services/api.service.js
+++ services/api.service.js
@@ -3,16 +3,33 @@ import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 const getWeather = async (city) => {
 
-	const token = await getKeyValue(TOKEN_DICTIONARY.token);
+	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 	if (!token) {
 		throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]');
 	}
 	
-	const { data } = await axios.get('http://api.openweathermap.org/geo/1.0/direct', {
+	const { data: dataGeo } = await axios.get('http://api.openweathermap.org/geo/1.0/direct', {
 		params: {
 			q: city,
 			appid: token,
 			limit: 1
+		}
+	});
+
+	if (dataGeo.length == 0) {
+		throw new Error('Неверно указан город');	
+	}
+
+	const lon = dataGeo[0].lon;
+	const lat = dataGeo[0].lat;
+
+	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+		params: {
+			lat: lat,
+			lon: lon,
+			appid: token,
+			lang: 'ru',
+			units: 'metric'
 		}
 	});
 
--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@
 import { getArgs } from './helpers/args.js';
 import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { saveKeyValue, getKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if(!token.length) {
@@ -17,23 +17,49 @@ const saveToken = async (token) => {
 	};	
 };
 
+const saveCity = async (city) => {
+	if(!city.length) {
+		printError('Не передан город');
+		return;
+	} 
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess('Город сохранен');
+	} catch (err) {
+		printError(err.message);
+	};	
+};
+
+const getForcast = async () => {
+	try {
+		const weather = await getWeather(await getKeyValue(TOKEN_DICTIONARY.city));
+		console.log(weather);
+	} catch (e) {
+		if(e?.response?.status == 404) {
+			printError('Неверно указан город');
+		} else if(e?.response?.status == 401) {
+			printError('Неверно указан токен');
+		} else {
+			printError(e.message);
+		}
+	}
+};
+
 const initCLI = () => {
 	const args = getArgs(process.argv);
-	console.log(process.env);
 	if (args.h) {
 		printHelp();
 	}
 
 	if (args.s) {
-		//saveKeyValue('city', args.s);
+		return saveCity(args.s);
 	}
 
 	if (args.t) {
 		return saveToken(args.t);		
 	}
 
-	// Вывести погоду
-	getWeather('Moscow');
+	getForcast();
 };
 
 initCLI();
